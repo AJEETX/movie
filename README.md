884609a1fb4d7922c6573f0cbcf3745f9db273e7
Ajeet <ajeet.tiwari@helloworld.com.au>
2018-06-01 07:03:56 +1000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# movie
-nodejs app with simple token authentication with async.parallel
+# movies
+
+
+ ![GitHub release](https://img.shields.io/github/release/ajeetx/movie.svg?style=for-the-badge) ![Maintenance](https://img.shields.io/maintenance/yes/2018.svg?style=for-the-badge)
+
+![Travis](https://img.shields.io/travis/AJEETX/movie.svg) |![GitHub Release Date](https://img.shields.io/github/release-date/ajeetx/movie.svg)|
+| --- | --- |
+
+[![nodejs](https://img.shields.io/badge/node-js-blue.svg?style=plastic)](https://nodejs.org/en/) | ![GitHub language count](https://img.shields.io/github/languages/count/ajeetx/movie.svg) | ![GitHub top language](https://img.shields.io/github/languages/top/ajeetx/movie.svg) |![GitHub repo size in bytes](https://img.shields.io/github/repo-size/ajeetx/movie.svg) 
+| ---          | ---        | ---      | ---        | 
+
+---------------------------------------
+## Introduction
+
+```
+simple nodejs app with simple token authentication with async.parallel.   
+```
+## Setup/Configuration
+
+Please install [![nodejs](https://img.shields.io/badge/node-js-blue.svg?style=plastic)](https://nodejs.org/en/) in order to run the application.
+
+The repository consists of a set of files including dependencies [node_modules folder].
+
+Kindly run the webjet.js file from command-line interface/terminal with following command with admin privilege: 
+
+```
+nodemon webjet.js
+```
+If the message comes up as "listening on port 3000". 
+
+The server has started successfully. All good.
+
+Please open a chrome browser and run "localhost:3000" url.
+
+Thanks...
+
+## Features
+-	simple token-based authentication
+-	search cheapest movie
+-	search movie by id 
+### Support or Contact
+
+Having any trouble? Check out our [documentation](https://github.com/AJEETX/movie/blob/master/README.md) or [contact support](mailto:ajeetkumar@email.com) and we’ll help you sort it out.
+
+
+[![HitCount](http://hits.dwyl.io/ajeetx/movieyy/projects/1.svg)](http://hits.dwyl.io/ajeetx/movie/projects/1) | ![GitHub contributors](https://img.shields.io/github/contributors/ajeetx/movie.svg?style=plastic)|![license](https://img.shields.io/github/license/ajeetx/movie.svg?style=plastic)|
+ | --- | --- | ---|
+
---
+++ webjet.js
@@ -0,0 +1,122 @@
+const express = require('express');
+const app=express();
+const async = require('async');
+const port=process.env.PORT || 9000;
+const bodyParser= require('body-parser');
+const tokenCode="sjd1HfkjU83ksdsm3802k";
+const cinema=[{id:1,title:'cine$11',price:11},{id:2,title:'cine$21',price:21}];
+const film=[{id:11,title:'film$12',price:12},{id:23,title:'film$2',price:2}];
+const users=[{  name:"webjet",   password:"webjet" }];
+
+app.use( bodyParser.json() );
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('./'));
+
+app.get('/', (req,res)=>{
+    res.sendFile('index.html');
+});
+
+app.post('/login',(req,res)=>{
+    var message;
+    for(var user of users){
+      if(user.name!=req.body.name){
+          message="Wrong Name";
+      }else{
+          if(user.password!=req.body.password){
+              message="Wrong Password";
+              break;
+          }
+          else{
+            var token=tokenCode;
+			      console.log("Login Successful. Token ="+token );			  
+              message="Login Successful";
+              break;
+          }
+      }
+    }
+    if(token){
+        res.status(200).json({
+            message,
+            token
+        });
+    }
+    else{
+        res.status(403).json({
+            message
+        });
+    }
+});
+
+app.use((req, res, next)=>{
+        console.log(req.body);
+        var token = req.body.token || req.query.token || req.headers['x-access-token'];
+        if(token){
+            if(token!=tokenCode){
+              res.status(403).json({
+                message:"Wrong Token"
+              });
+            }
+            else{
+              req.decoded=tokenCode;
+              next();
+            }
+        }
+        else{
+          res.status(403).json({
+            message:"No Token"
+          });
+        }
+});
+
+app.post('/movie',(req,res)=>{
+  let cheapest;
+    async.parallel({
+        cine: function(callback) {callback(null, cinema);},
+        film: function(callback) {callback(null, film);}
+      }, function(err, results) {
+        let data=results.cine.concat(results.film);
+        cheapest=data.reduce((min, p) => p.price < min ? p.price : min, data[0].price);
+        let movie=data.find(function(o){ return o.price == cheapest; })
+        res.writeHead(200, {"Content-Type": "application/json"});
+        console.log(movie);
+       res.end(JSON.stringify(movie));
+      });
+    });
+
+app.post('/movie/:id',(req,res)=>{
+  let movie;
+  const Id=req.params.id;
+  console.log('Id = '+Id);
+  async.parallel({
+      ci:function(callback) {
+        let cmovie=cinema.filter(m=>m.id==Id);
+        if(cmovie.length>0){
+          movie=cmovie[0].title;
+        }
+        callback(null,{});
+      },
+      fi:function(callback) {
+        let fmovie=film.filter(m=>m.id===Id);
+        if(fmovie.length>0){
+          movie=film.filter(m=>m.id===Id)[0].title;
+        }
+        callback(null,{});
+      }
+    }, function(err, results) {
+      res.writeHead(200, {"Content-Type": "application/json"});
+      if(err){            
+        return console.log(err);
+      }
+      if(movie){
+        console.log(movie);
+        }else{
+          console.log('not found');
+        }
+      res.end(JSON.stringify(movie));
+    });
+  });
+   
+
+app.listen(3000, function(){ console.log(`listening on port ${port}`);});
